0840066eb15c8b63f992aaab82b976f38e9c1444
Mateusz Ledzewicz <mateusz@cobiro.com>
2022-06-30 06:29:48 +0200

    initial commit

---
+++ apps/nursa/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hexagon-training-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nursa';
+}
